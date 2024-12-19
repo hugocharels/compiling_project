e31eeea498f4380e39402a0147591e9c93693182
hugocharels <hugocharels@hotmail.com>
2024-12-19 18:52:28 +0100

    :construction: ConditionComponent almost done

--- part3/src/compiler/code/ConditionComponent.java
+++ part3/src/compiler/code/ConditionComponent.java
@@ -1,11 +1,48 @@
 package compiler.code;
 
+import compiler.GlsTerminal;
 import compiler.ParseTree;
 
 public interface ConditionComponent extends CodeComponent {
 
 	static ConditionComponent fromParseTree(ParseTree parseTree) {
-		// TODO: Implement this
+		//           <Cond>
+		//          /     \
+		// <CondSimple>   <NextCond>
+
+		// <CondSimple> -> | <Cond> |
+		//              -> <Expr> <Comp> <Expr>
+
+		// <NextCond> -> -> <CondSimple> <NextCond>
+		//            -> ε
+
+		ParseTree condSimple = parseTree.getChild(0);
+		ParseTree nextCond = parseTree.getChild(1);
+
+		ConditionComponent leftCond;
+
+		// CondSimple -> | <Cond> |
+		if (condSimple.getChild(0).getLabel().equals(GlsTerminal.PIPE)) {
+			// | <Cond> |
+			// 0   1    2
+			leftCond = new PipeCondNode(ConditionComponent.fromParseTree(condSimple.getChild(1)));
+			// CondSimple -> <Expr> <Comp> <Expr>
+		} else {
+			// <Expr> <Comp> <Expr>
+			//   0      1      2
+			ExprComponent left = ExprComponent.fromParseTree(condSimple.getChild(0));
+			String op = condSimple.getChild(1).getLexicalSymbol().getValue().toString();
+			ExprComponent right = ExprComponent.fromParseTree(condSimple.getChild(2));
+			leftCond = new ExprCondNode(left, op, right);
+		}
+
+		// NextCond -> ε
+		if (nextCond.getChild(0).getLabel().equals(GlsTerminal.EPSILON)) {
+			return leftCond;
+		}
+
+		// NextCond -> -> <CondSimple> <NextCond>
+		// TODO: Implement this case
 		return null;
 	}
 }
--- part3/src/compiler/code/ExprComponent.java
+++ part3/src/compiler/code/ExprComponent.java
@@ -5,7 +5,25 @@ import compiler.ParseTree;
 public interface ExprComponent extends CodeComponent {
 
 	static ExprComponent fromParseTree(ParseTree parseTree) {
-		// TODO: Implement this
+		// ExprArith -> [VarName] | [Number]
+		//            -> ( <ExprArith> )
+		//            -> - <ExprArith>
+		//            -> <ExprArith> <Op> <ExprArith>
+
+		// <ProdArith> <ExprArith'>
+		//    0            1
+
+		ParseTree prodArith = parseTree.getChild(0);
+		ParseTree exprArithPrime = parseTree.getChild(1);
+
+		// If it is a number or a variable name
+//		if (
+//				exprArithPrime.getChild(0).getLexicalSymbol().getType().toGlsTerminal().equals(GlsTerminal.EPSILON) &&
+//				true
+//		) {
+//			// TODO
+//		}
+
 		return null;
 	}
 }
